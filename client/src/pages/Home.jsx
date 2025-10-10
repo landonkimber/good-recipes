c9ce7740cf876b71918721cb34c71f5fa98a50e2
landonkimber <landonkimber33@gmail.com>
2025-10-10 15:58:18 -0600

    added mobile response to homepage hero

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -18,23 +18,19 @@ const Home = () => {
 
         {/* Overlay Content Box */}
         <div className="relative z-10 h-full flex items-center justify-center px-6">
-          <div className="bg-gray-800/50 h-[50%] backdrop-blur-md rounded-md flex flex-row w-full max-w-7xl p-8 md:p-12 gap-8">
+          <div className="bg-gray-800/50 h-[60%] md:h-[50%] backdrop-blur-md rounded-md flex flex-col md:flex-row sm:flex-col w-full max-w-7xl p-4">
             {/* Left: Logo + Title */}
-            <div className="flex flex-col items-center justify-center w-1/2 text-sky-900 gap-6">
-              <img
-                src={ChefLogo}
-                alt="Chef Logo"
-                className="w-32 h-32 md:w-36 md:h-36"
-              />
+            <div className="flex flex-col md:w-[50%] sm:w-full items-center justify-center text-sky-900">
+              <img src={ChefLogo} alt="Chef Logo" className="w-32 h-32" />
 
-              <h1 className="text-5xl md:text-6xl font-lobster text-sky-100 text-center">
+              <h1 className="text-3xl lg:text-5xl sm:text-3xl font-lobster text-sky-100 text-center my-4">
                 Good Recipes
               </h1>
             </div>
-            <div className="h-full w-1 bg-sky-900 rounded-full"></div>
+            <div className="w-full h-1 mb-2 md:h-full sm:w-full md:w-1 sm:h-1 bg-sky-900 rounded-full"></div>
             {/* Right: Intro Text */}
-            <div className="md:w-1/2 flex items-center justify-center text-center md:text-left">
-              <p className="text-lg md:text-xl font-roboto text-sky-100 drop-shadow-sm">
+            <div className="flex w-full h-1/2 md:h-full md:w-[50%] sm:w-full items-center justify-center text-center">
+              <p className="text-sm sm:text-base md:text-lg lg:text-xl font-roboto text-sky-100">
                 Discover comforting meals, rich flavors, and easy-to-follow
                 recipes! From quick weeknight dinners to indulgent weekend
                 feasts, Good Recipes has foods to satisfy every at home chef!
